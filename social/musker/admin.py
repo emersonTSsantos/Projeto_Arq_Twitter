cd6ab98cf199109e8f5b42b7094d15fe1f5869c5
Emerson Teixeira <emerson@exemplo.com>
2025-07-14 17:52:00 -0300

    Criando Usuários, Perfil e Seguidores

--- social/musker/admin.py
+++ social/musker/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import UserProfile
 
-# Register your models here.
+
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Mix Profile info into User info
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username field on admin page
+    fields = ['username']
+    inlines = [UserProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Reregister User and Profile
+admin.site.register(User, UserAdmin)
+# admin.site.register(UserProfile)
+
+
+
